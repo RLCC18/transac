e700d9868f810276e90580ba3cfc59d313c8ba0a
BillyWilder44 <124748839+BillyWilder44@users.noreply.github.com>
2023-05-08 15:20:00 +0800

    display added with index

---
+++ display.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet"  type="text/css" href="index.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-light bg-#FFEBC1">
+      <span class="navbar-brand mb-0 h1 ">display</span>
+    </nav>
+    <section>
+      <div class="container h-100 d-flex">
+        <div class="jumbotron my-auto">
+          <h1 class="display-3">Queue Num: 301</h1>
+
+        </div>
+    </div>
+    <div class="note small bg-light p-2">
+        I love you Rose with all my heart. You're the only person i've loved.
+    </div>
+
+
+  </section>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+  </body>
+</html>
