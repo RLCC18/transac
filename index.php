2178a03eb4b68090bd099cf79b5f631b7bb2c538
PwnedSoul39 <107659960+PwnedSoul39@users.noreply.github.com>
2023-05-12 22:10:46 +0800

    Updated some minor stuff (again)

--- index.php
+++ index.php
@@ -5,29 +5,28 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Queueing System</title>
-    <link rel="stylesheet" type="text/css"  href="./css/bootstrap.min.css">
-    <link href="vendor/css/all.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css"  href="./resource/css/transaction.css">
     <script src="https://kit.fontawesome.com/9622798f89.js"></script>
-
+    <title>Queueing System</title>
   </head>
   <body>
 
     <section>
       <nav class="navbar nav-dash">
-        <a href="./queue.php">Dashboard</a>
+        <a href="./queue.php" target="_blank">Dashboard</a>
       </nav>
-
+      
+      <?php user_valid(); ?>
       <div class="container-fluid">
         <div class="row">
           <div class="card">
-            <?php user_valid(); ?>
+            <div class="pl-5 pr-5">
+            </div>
             <form class="form mt-5 mb-2" method="post">
                 <div class="form-row">
-                  <input class="form-control m-5" placeholder="Enter your name" name="userbox">
+                  <input class="form-control mb-5 ml-5 mr-5" placeholder="Enter your name" name="userbox">
                 </div>
                 <div class="transact">
                   <button class="btn btn-submit" type="submit" name="withbtn">
@@ -57,7 +56,7 @@
       </footer>
 
     </section>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
   </body>
 </html>
