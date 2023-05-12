426e4030fa1df693e47e4e2dfa64d5a6cc4aa6f8
PwnedSoul39 <107659960+PwnedSoul39@users.noreply.github.com>
2023-05-12 21:53:13 +0800

    Minor edits for compatibility

--- index.php
+++ index.php
@@ -3,48 +3,61 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-        <link rel="stylesheet" href="./resource/css/main.css">
-        <title>Transaction</title>
-    </head>
-    <body>
-        <!-- 
-            Please note na tinetest ko lng kung gumagana ung code, baguhin nyo na lng ✌ 
-        -->
-        <header class="row-fluid">
-            <nav class="navbar navbar-expand bg-dark">
-                <span class="navbar-brand text-white">Aughh</span>
-                <ul class="ml-auto navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="queue.php" target="_blank">Dashboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="display.php" target="_blank">Display</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Queueing System</title>
+    <link rel="stylesheet" type="text/css"  href="./css/bootstrap.min.css">
+    <link href="vendor/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"  href="./resource/css/transaction.css">
+    <script src="https://kit.fontawesome.com/9622798f89.js"></script>
 
-        <div class="container">
+  </head>
+  <body>
+
+    <section>
+      <nav class="navbar nav-dash">
+        <a href="./queue.php">Dashboard</a>
+      </nav>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="card">
             <?php user_valid(); ?>
             <form class="form mt-5 mb-2" method="post">
-                <div class="form-group">
-                <input class="form-control" placeholder="Enter your name" name="userbox">
+                <div class="form-row">
+                  <input class="form-control m-5" placeholder="Enter your name" name="userbox">
                 </div>
-                <button class="btn btn-primary" type="submit" name="withbtn">
-                    WITHDRAW
-                </button>
-                <button class="btn btn-primary" type="submit" name="depobtn">
-                    DEPOSIT
-                </button>
+                <div class="transact">
+                  <button class="btn btn-submit" type="submit" name="withbtn">
+                      WITHDRAW
+                  </button>
+                  <button class="btn btn-submit" type="submit" name="depobtn">
+                      DEPOSIT
+                  </button>
+                </div>  
             </form>
-        </div>        
+          </div>
+        </div>
+      </div> 
 
-        <!-- Bootstrap -->
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    </body>
+      <footer id="sticky-footer" class="py-3 bg-dark text-white-50 fixed-bottom slide-in-right">
+        <div class="container text-center">
+          <div class="row">
+            <div class="col col-sm-5 text-left text-light">
+              <small>Copyright &copy;Westview University     All Rights Reserved 2023</small>
+            </div>
+            <div class="col text-right text-light">
+              <small>Group 4: Marcus Bustos, Ralph Cruz, Angelique Gabriel, Krizia Lleva, Roderick Nucup, Emman Siva.</small>
+            </div>
+          </div>
+        </div>
+
+      </footer>
+
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+  </body>
 </html>
