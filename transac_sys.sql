55ff550bdf4569665bc84ff9fa6a9315a56a54a1
PwnedSoul39 <107659960+PwnedSoul39@users.noreply.github.com>
2023-05-12 10:25:00 +0800

    Updated the sql for importing

--- transac_sys.sql
+++ transac_sys.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: May 11, 2023 at 08:11 AM
+-- Generation Time: May 12, 2023 at 02:24 AM
 -- Server version: 10.10.2-MariaDB
 -- PHP Version: 8.0.26
 
@@ -34,6 +34,7 @@ CREATE TABLE IF NOT EXISTS `tbl_ts` (
   `ts_action` varchar(255) NOT NULL,
   `ts_qnum` varchar(255) NOT NULL,
   `ts_status` varchar(255) NOT NULL DEFAULT 'PENDING',
+  `ts_date` datetime DEFAULT NULL,
   PRIMARY KEY (`ts_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
 
@@ -41,15 +42,15 @@ CREATE TABLE IF NOT EXISTS `tbl_ts` (
 -- Dumping data for table `tbl_ts`
 --
 
-INSERT INTO `tbl_ts` (`ts_id`, `ts_user`, `ts_action`, `ts_qnum`, `ts_status`) VALUES
-(1, 'Juan A. Dela Cruz', 'Withdraw', 'W-1', 'DISPLAY'),
-(2, 'Jerome B. Manalo', 'Deposit', 'D-1', 'NEXT'),
-(6, 'Elias Perez', 'Withdraw', 'W-9484', 'PENDING'),
-(3, 'A', 'Withdraw', 'W-920', 'PENDING'),
-(4, 'D', 'Deposit', 'D-695', 'PENDING'),
-(5, 'Jerry Miller', 'Deposit', 'D-767', 'PENDING'),
-(7, 'Elias Perez', 'Deposit', 'D-2225', 'PENDING'),
-(8, 'Elton Brand', 'Withdraw', 'W-2046', 'DISPLAY');
+INSERT INTO `tbl_ts` (`ts_id`, `ts_user`, `ts_action`, `ts_qnum`, `ts_status`, `ts_date`) VALUES
+(1, 'Juan A. Dela Cruz', 'Withdraw', 'W-1', 'NEXT', NULL),
+(2, 'Jerome B. Manalo', 'Deposit', 'D-1', 'DISPLAY', '2023-05-12 10:21:55'),
+(6, 'Elias Perez', 'Withdraw', 'W-9484', 'PENDING', NULL),
+(3, 'A', 'Withdraw', 'W-920', 'PENDING', NULL),
+(4, 'D', 'Deposit', 'D-695', 'DISPLAY', '2023-05-12 10:21:06'),
+(5, 'Jerry Miller', 'Deposit', 'D-767', 'PENDING', NULL),
+(7, 'Elias Perez', 'Deposit', 'D-2225', 'PENDING', NULL),
+(8, 'Elton Brand', 'Withdraw', 'W-2046', 'NEXT', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
